3c3f218ea47ca07997e6feda2c63922aa30a3191
Mark Chorniy <12marik12@gmail.com>
2023-03-04 18:57:57 +0200

    Brbrbrbr

--- GMKIOS_Lab_1/GMKIOS_Lab_1.cpp
+++ GMKIOS_Lab_1/GMKIOS_Lab_1.cpp
@@ -4,91 +4,17 @@
 
 using namespace sf;
 
-class ShapeObj
-{
-public:
-	ShapeObj();
-	~ShapeObj();
-
-private:
-
-};
-
-ShapeObj::ShapeObj()
-{
-}
-
-ShapeObj::~ShapeObj()
-{
-}
-
-class ShapeGrid
-{
-public:
-	ShapeGrid(float circumscribed_r, int anglescount, float angle );
-	~ShapeGrid();
-
-	void set_circumscribed_r(float r) {
-		if (r < 10.0f) {
-			this->circumscribed_r = 10.0f;
-		}
-		else {
-			this->circumscribed_r = r;
-		}
+float circletouch(float radius, Vector2i mousepoint, Vector2f targetpoint, float min = 1, float max = 5) {
+	float distance = sqrt(pow(mousepoint.x - targetpoint.x, 2.0f) + pow(mousepoint.y - targetpoint.y, 2.0f));
+	if (distance < 50.0f) {
+		return min;
 	}
-
-	float get_circumscribed_r() {
-		return circumscribed_r;
+	else if (distance > radius) {
+		return max;
 	}
-
-	void set_anglescount(int anglescount) {
-		if (anglescount < 3) {
-			this->anglescount = 3;
-		}
-		else if (anglescount > 6) {
-			this->anglescount = 6;
-		}
-		else {
-			this->anglescount = anglescount;
-		}
+	else {
+		return min + (max - min) * distance / radius;
 	}
-
-	float get_anglescount() {
-		return anglescount;
-	}
-
-	void set_angle(float angle) {
-		this->angle = angle;
-	}
-
-	float get_angle() {
-		return angle;
-	}
-
-	void set_limits(int x = 640, int y = 480) {
-		this->limitx = x;
-		this->limity = y;
-	}
-
-	
-
-private:
-	float circumscribed_r;
-	int anglescount;
-	float angle;
-	int limitx;
-	int limity;
-};
-
-ShapeGrid::ShapeGrid(float circumscribed_r = 50.0f, int anglescount = 6, float angle = 0.0f)
-{
-	this->set_circumscribed_r(circumscribed_r);
-	this->set_anglescount(anglescount);
-	this->set_angle(angle);
-}
-
-ShapeGrid::~ShapeGrid()
-{
 }
 
 
@@ -96,44 +22,54 @@ int main()
 {
 	sf::ContextSettings settings = sf::ContextSettings();
 	settings.antialiasingLevel = 16;
-	RenderWindow window(VideoMode(640, 480), "Udachi na interview", 7U, settings);
+	RenderWindow window(VideoMode(1920, 1080), "Lab 1", 7U, settings);
 	window.setFramerateLimit(60);
+	bool lock = false;
+
+	sf::RenderTexture text;
+	text.create(window.getSize().x, window.getSize().y);
+
+	sf::Clock clock;
 
 	while (window.isOpen())
 	{
 
 		sf::Vector2i position = sf::Mouse::getPosition(window);
 
-		std::cout << position.x << " " << position.y << "\n";
+
+
 
 		Event event;
 		while (window.pollEvent(event))
 		{
 
-			if (event.type == Event::Closed)
 
+			if (event.mouseButton.button == sf::Mouse::Left) {
+
+			}
+
+			if (event.type == Event::Closed)
 				window.close();
 		}
 
-		ConvexShape convex;
 
 		window.clear(Color(255, 255, 255, 0));
 
 		float circleradius = 50.0f;
 		float distance = circleradius * sqrt(3.0f);
-		CircleShape hexagon(circleradius, 4);
+		CircleShape hexagon(circleradius, 6);
 		hexagon.setFillColor(Color::Transparent);
 		hexagon.setOutlineColor(Color::Black);
 		hexagon.setOutlineThickness(1.0f);
 
-		for (int i = 0; i < 10; i++) {
-			for (int y = 0; y < 9; y++) {
-				hexagon.setPosition(-50 + i * distance + ((y + 1) % 2 * distance / 2), y * circleradius * 1.5 - 50);
 
-				if (hexagon.getGlobalBounds().contains(position.x, position.y) ) {
-					hexagon.setOutlineThickness(3.0f);
-				}
+		for (int i = 0; i < 1 + window.getSize().x / distance; i++) {
+			for (int y = 0; y < 2 + window.getSize().y / (distance / 1.5); y++) {
+				hexagon.setPosition(-circleradius + i * distance + ((y + 1) % 2 * distance / 2), y * circleradius * 1.5 - circleradius);
 
+				//if (hexagon.getGlobalBounds().contains(position.x, position.y) ) {
+				//	hexagon.setOutlineThickness(3.0f);
+				//}
 
 				window.draw(hexagon);
 				hexagon.setOutlineThickness(1.0f);
@@ -143,19 +79,22 @@ int main()
 		hexagon.setRadius(35.0f);
 		hexagon.setOutlineThickness(5.0f);
 
-		for (int i = 0; i < 10; i++) {
-			for (int y = 0; y < 9; y++) {
-				hexagon.setPosition(-50 + i * distance + ((y + 1) % 2 * distance / 2) + 15.0f, y * circleradius * 1.5 - 50 + 15.0f);
+		for (int i = 0; i < 1 + window.getSize().x / distance; i++) {
+			for (int y = 0; y < 2 + window.getSize().y / (distance / 1.5); y++) {
+
+				hexagon.setPosition(-circleradius + i * distance + ((y + 1) % 2 * distance / 2) + (circleradius - hexagon.getRadius()), y * circleradius * 1.5 - circleradius + (circleradius - hexagon.getRadius()));
+				hexagon.setOutlineThickness(abs(sin(clock.getElapsedTime().asSeconds() + i / 6.0f + i / 4 + y / 6.0f) * 10) + 1);
 				window.draw(hexagon);
 			}
 		}
 
-		hexagon.setRadius(25.0f);
-		hexagon.setOutlineThickness(1.0f);
+		hexagon.setRadius(25.0f + sin(clock.getElapsedTime().asSeconds()) * 5);
+		hexagon.setOutlineThickness(1);
 
-		for (int i = 0; i < 10; i++) {
-			for (int y = 0; y < 9; y++) {
-				hexagon.setPosition(-50 + i * distance + ((y + 1) % 2 * distance / 2) + 25.0f, y * circleradius * 1.5 - 50 + 25.0f);
+		for (int i = 0; i < 1 + window.getSize().x / distance; i++) {
+			for (int y = 0; y < 2 + window.getSize().y / (distance / 1.5); y++) {
+				hexagon.setPosition(-circleradius + i * distance + ((y + 1) % 2 * distance / 2) + (circleradius - hexagon.getRadius()), y * circleradius * 1.5 - circleradius + (circleradius - hexagon.getRadius()));
+				hexagon.setOutlineThickness(abs(sin(clock.getElapsedTime().asSeconds() + i / 3 + y / 3)));
 				window.draw(hexagon);
 			}
 		}
@@ -163,22 +102,28 @@ int main()
 		hexagon.setRadius(10.0f);
 		hexagon.setOutlineThickness(5.0f);
 
-		for (int i = 0; i < 10; i++) {
-			for (int y = 0; y < 9; y++) {
-				hexagon.setPosition(-50 + i * distance + ((y + 1) % 2 * distance / 2) + 40.0f, y * circleradius * 1.5 - 50 + 40.0f);
+		for (int i = 0; i < 1 + window.getSize().x / distance; i++) {
+			for (int y = 0; y < 2 + window.getSize().y / (distance / 1.5); y++) {
+				hexagon.setPosition(-circleradius + i * distance + ((y + 1) % 2 * distance / 2) + (circleradius - hexagon.getRadius()), y * circleradius * 1.5 - circleradius + (circleradius - hexagon.getRadius()));
+				float thickness = circletouch(250.0f, position, hexagon.getPosition());
+				hexagon.setOutlineThickness(thickness);
 				window.draw(hexagon);
 			}
 		}
 
 		hexagon.setRadius(5.0f);
-		hexagon.setOutlineThickness(1.0f);
+		hexagon.setOutlineThickness(5.0f);
 
-		for (int i = 0; i < 10; i++) {
-			for (int y = 0; y < 9; y++) {
-				hexagon.setPosition(-50 + i * distance + ((y + 1) % 2 * distance / 2) + 45.0f, y * circleradius * 1.5 - 50 + 45.0f);
+
+		for (int i = 0; i < 1 + window.getSize().x / distance; i++) {
+			for (int y = 0; y < 2 + window.getSize().y / (distance / 1.5); y++) {
+				hexagon.setPosition(-circleradius + i * distance + ((y + 1) % 2 * distance / 2) + (circleradius - hexagon.getRadius()), y * circleradius * 1.5 - circleradius + (circleradius - hexagon.getRadius()));
 				window.draw(hexagon);
 			}
 		}
+
+
+
 
 
 		window.display();
